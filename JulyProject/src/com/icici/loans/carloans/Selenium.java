4ed52a95db25d236cb212f61331f9884ca7d0569
HarshNaik02 <56308838+HarshNaik02@users.noreply.github.com>
2019-10-08 13:45:51 -0400

    Update Selenium.java

--- JulyProject/src/com/icici/loans/carloans/Selenium.java
+++ JulyProject/src/com/icici/loans/carloans/Selenium.java
@@ -2,6 +2,10 @@ package com.icici.loans.carloans;
 
 public class Selenium {
 
+	Public void add()
+	{
+		int a,b,d;
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Selenium Program");
