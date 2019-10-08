bf68305b40df24dba4c338768c6da1492fb47e88
HarshNaik02 <Harshnaik002@gmail.com>
2019-10-08 13:08:56 -0400

    Added line in Selenium

--- JulyProject/src/com/icici/loans/carloans/Selenium.java
+++ JulyProject/src/com/icici/loans/carloans/Selenium.java
@@ -3,7 +3,8 @@ package com.icici.loans.carloans;
 public class Selenium {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Selenium Program");
 
 	}
 
